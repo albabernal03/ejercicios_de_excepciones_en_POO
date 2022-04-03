4b5798916bdd32adddcb9c9ac710e717a3b5bbaf
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-04-03 15:47:30 +0000

    defino las variables dentro del constructor

--- clases/email.py
+++ clases/email.py
@@ -1,3 +1,7 @@
 import re
 class inicio_sesion():
-  def __init__(self, direccion_correo, intentos, intentosMax, nombre)
+  def __init__(self, direccion_correo, intentos, intentosMax, nombre):
+    self.direccion_correo: direccion_correo
+    self.intentos = intentos
+    self.intentosMax= intentosMax
+    self.nombre= nombre
